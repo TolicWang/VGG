6c00a73267b036c8cc0231883e12c51dc1459e34
tolic <wangchengo@126.com>
2018-09-13 08:45:16 +0800

    增加eval模块

---
+++ VGG16/eval.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from cnn import VGG16
+import os,sys
+from config import FLAGS
+
+current_dir = os.path.abspath('../')
+sys.path.append(current_dir)
+from data_helper import gen_train_or_test_batch, load_data_cifar
+
+
+vgg16 = VGG16(is_training=False,keep_prob=1)
+with tf.Session() as sess:
+    saver = tf.train.Saver()
+    saver.restore(sess, tf.train.latest_checkpoint(FLAGS.model_save_path))
+    x_train, y_train, _ = load_data_cifar('../data/cifar-100-python/test')
+    correct_sum = 0
+    for i in range(100):
+        batch_x, batch_y = gen_train_or_test_batch(x_train, y_train, i, 100)
+        feed_dic={vgg16.input_x:batch_x,vgg16.input_y:batch_y}
+        prediction = sess.run(vgg16.predictions,feed_dict=feed_dic)
+        correct_sum += tf.reduce_sum(tf.cast(prediction, tf.float32))
+    print('Acc on test dataset:',sess.run(correct_sum/10000))
+
