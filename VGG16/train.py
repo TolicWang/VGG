b9d2e35c932cdd1872819544594821a9753333f5
tolic <wangchengo@126.com>
2018-09-19 08:36:30 +0800

    加入验证集和BN

--- VGG16/train.py
+++ VGG16/train.py
@@ -7,7 +7,7 @@ import numpy as np
 
 current_dir = os.path.abspath('../')
 sys.path.append(current_dir)
-from data_helper import gen_train_or_test_batch, load_data_cifar
+from data_helper import gen_train_or_test_batch, load_data_cifar,data_augmentation
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # 指定一个GPU
 print('\n----------------Parameters--------------')  # 在网络训练之前，先打印出来看看
@@ -15,9 +15,9 @@ for attr, value in sorted(FLAGS.__flags.items()):
     print('{}={}'.format(attr.upper(), value))
 print('----------------Parameters--------------\n')
 x, y, _ = load_data_cifar()
-x_train,y_train = x[:40000],y[:40000]
-x_dev,y_dev = x[-10000:],y[-10000:]
-
+x_train, y_train = x[:40000], y[:40000]
+x_dev, y_dev = x[-10000:], y[-10000:]
+print("train/dev: {}/{} !".format(len(y_train), len(y_dev)))
 
 with tf.Graph().as_default():
     session_conf = tf.ConfigProto(allow_soft_placement=FLAGS.allow_soft_placement)
@@ -26,15 +26,12 @@ with tf.Graph().as_default():
     sess = tf.Session(config=session_conf)
     with sess.as_default():
         vgg16 = VGG16()
+        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         global_step = tf.Variable(0, trainable=False)
-        with tf.device('/gpu:0'):
-            optimaizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
-            # train_step = tf.train.GradientDescentOptimizer(
-            #     FLAGS.learning_rate).minimize(loss=vgg16.loss, global_step=global_step)
-            # grad = optimaizer.compute_gradients(loss=vgg16.loss)
-            train_step = optimaizer.minimize(loss=vgg16.loss, global_step=global_step)
-
-            # train_step = tf.train.AdamOptimizer(1e-3).minimize(loss=cnn.loss, global_step=global_step)
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            with tf.device('/gpu:0'):
+                optimaizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
+                train_step = optimaizer.minimize(loss=vgg16.loss, global_step=global_step)
     saver = tf.train.Saver()
     if os.path.exists(FLAGS.model_save_path + 'checkpoint'):
         saver.restore(sess, tf.train.latest_checkpoint(FLAGS.model_save_path))
@@ -46,17 +43,18 @@ with tf.Graph().as_default():
     for i in range(999999900000):
 
         batch_x, batch_y = gen_train_or_test_batch(x_train, y_train, i, FLAGS.batch_size)
-        feed_dic = {vgg16.input_x: batch_x, vgg16.input_y: batch_y}
+        batch_x = data_augmentation(batch_x)
+        feed_dic = {vgg16.is_training: True, vgg16.input_x: batch_x, vgg16.input_y: batch_y}
         _, loss, acc = sess.run([train_step, vgg16.loss, vgg16.accuracy], feed_dict=feed_dic)
 
-        if (i % 50) == 0:
+        if (i+1 % 50) == 0:
             now = datetime.datetime.now()
-            print('loss:%.7f,acc on train :%.7f---time:%s'%(loss, acc, now - last))
+            print('loss:%.7f,acc on train :%.7f---time:%s' % (loss, acc, now - last))
             last = now
-        if (i%1300) ==0:
-            print('========> epoches:',epoches)
-            epoches +=1
-        if (i % FLAGS.save_freq) == 0:
+        if (i+1  % 1300) == 0:
+            print('========> epoches:', epoches)
+            epoches += 1
+        # if (i % FLAGS.save_freq) == 0:
             print('Iterations:  ', i)
             saver.save(sess, os.path.join(FLAGS.model_save_path,
                                           FLAGS.model_name),
@@ -64,7 +62,7 @@ with tf.Graph().as_default():
             correct_sum = 0
             for i in range(100):
                 batch_x, batch_y = gen_train_or_test_batch(x_dev, y_dev, i, 100)
-                feed_dic = {vgg16.input_x: batch_x, vgg16.input_y: batch_y}
+                feed_dic = {vgg16.is_training: False, vgg16.input_x: batch_x, vgg16.input_y: batch_y}
                 prediction = sess.run(vgg16.predictions, feed_dict=feed_dic)
-                correct_sum += np.sum(prediction*1)
+                correct_sum += np.sum(prediction * 1)
             print('Acc on dev dataset:', (correct_sum / 10000))
