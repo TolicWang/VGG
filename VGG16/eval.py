40567d31099202bf5471dc78fea0baa0e11e2df4
tolic <wangchengo@126.com>
2018-09-19 08:35:13 +0800

    修改VGG16()的参数传递

--- VGG16/eval.py
+++ VGG16/eval.py
@@ -8,7 +8,7 @@ current_dir = os.path.abspath('../')
 sys.path.append(current_dir)
 from data_helper import gen_train_or_test_batch, load_data_cifar
 
-vgg16 = VGG16(is_training=False, keep_prob=1)
+vgg16 = VGG16(keep_prob=1)
 with tf.Session() as sess:
     saver = tf.train.Saver()
     saver.restore(sess, tf.train.latest_checkpoint(FLAGS.model_save_path))
@@ -16,7 +16,7 @@ with tf.Session() as sess:
     correct_sum = 0
     for i in range(100):
         batch_x, batch_y = gen_train_or_test_batch(x_train, y_train, i, 100)
-        feed_dic = {vgg16.input_x: batch_x, vgg16.input_y: batch_y}
+        feed_dic = {vgg16.is_training: False, vgg16.input_x: batch_x, vgg16.input_y: batch_y}
         prediction = sess.run(vgg16.predictions, feed_dict=feed_dic)
         correct_sum += np.sum(prediction * 1)
 
